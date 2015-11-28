a2c86fb2ebe7dffad236b375cd24ed695d0e7afd
Educacion.be <guzmanhilario2009@hotmail.com>
2015-11-28 10:44:53 -0500

    Update head.php

--- theme/aardvark/layout/head.php
+++ theme/aardvark/layout/head.php
@@ -15,8 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /*
- * @author    Shaun Daubney
- * @package   theme_aardvark
+ * @author    Hilario Guzman
+ * @package   theme_educacionbe
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 ?>
