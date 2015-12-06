6f3edb8d649057148248bd19b4ede826b002f824
campus <educacion.be@hotmail.com>
2015-12-05 21:23:58 -0500

    Autentificacion
    Autentificacion

---
+++ blocks/autoattend/version.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$plugin->requires  = 2011120500;			// Moodle 2.2
+$plugin->component = 'block_autoattend';	// Full name of the plugin (used for diagnostics)
+$plugin->cron      = 0;
+$plugin->maturity  = MATURITY_STABLE;
+
+$plugin->version   = 2015120100;			// The current plugin version (Date: YYYYMMDDXX)
+
+$plugin->release   = '2.4.4';
+
